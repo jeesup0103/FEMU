01bcec7286b3c02cfabfbf414ce112a5b5ea6981
jeesup0103 <jeesup0103@gmail.com>
2024-12-11 22:33:36 +0900

    return lun if

--- hw/femu/bbssd/ftl.c
+++ hw/femu/bbssd/ftl.c
@@ -262,7 +262,7 @@ static void ssd_advance_ru_write_pointer(struct ssd *ssd, uint16_t rgid, uint16_
             ru->wp.ch = start_lunidx / spp->luns_per_ch;
             // ru->wp.ch = 0;
             ru->wp.lun++;
-            if (ru->wp.lun == (rgid + 1) * RG_DEGREE % spp->luns_per_ch)
+            if (ru->wp.lun == spp->luns_per_ch)
             {
                 ru->wp.lun = start_lunidx % spp->luns_per_ch;
                 ru->wp.pg++;
@@ -330,7 +330,7 @@ static void ssd_advance_ru_write_pointer(struct ssd *ssd, uint16_t rgid, uint16_
             ru->wp.lun++;
             if (ru->wp.lun == (rgid + 1) * RG_DEGREE % spp->luns_per_ch)
             {
-                ru->wp.lun = start_lunidx % spp->luns_per_ch;
+                ru->wp.lun == spp->luns_per_ch;
                 ru->wp.pg++;
                 // RU is full
                 if (ru->wp.pg >= spp->pgs_per_ru)
