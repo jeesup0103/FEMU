26473dfd6c378e24957f4ab0d3e56e5a77fb20b9
jeesup0103 <jeesup0103@gmail.com>
2024-12-12 23:31:16 +0900

    debug

--- hw/femu/bbssd/ftl.c
+++ hw/femu/bbssd/ftl.c
@@ -305,9 +305,10 @@ static void ssd_advance_ru_write_pointer(struct ssd *ssd, uint16_t rgid, uint16_
                     // Update GC RU
                     rum->ii_gc_ruid = ruid;
                 }
-                // else{
-                //     ruh->cur_ruids[rgid] = ruid;
-                // }
+                else{
+                    printf("Changing ruh\n");
+                    ruh->cur_ruids[rgid] = ruid;
+                }
             }
         }
     }
