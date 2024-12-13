80deaa23dfe06d748718beedd9efac2a646c085b
jeesup0103 <jeesup0103@gmail.com>
2024-12-13 11:29:24 +0900

    ph bit

--- hw/femu/bbssd/ftl.c
+++ hw/femu/bbssd/ftl.c
@@ -1042,9 +1042,9 @@ static uint64_t ssd_write(struct ssd *ssd, NvmeRequest *req)
 	uint16_t pid = rw->dspec;                   // Placement ID -> specifies a RG and placement handle referencing a unique RU
 	uint16_t rgif = endgrp->fdp.rgif;           // RG id format
     // uint16_t ph_bits = 16 - rgif;
-    // uint16_t rgid = pid >> ph_bits;      // RG id
-    uint16_t rgid = pid >> 16 - rgif;              // RG id
-    uint16_t ph_bits = MAX_RUHS;
+    // uint16_t rgid = pid >> ph_bits;          // RG id
+    uint16_t rgid = pid >> (16 - rgif);         // RG id
+    uint16_t ph_bits = log(NVME_FDP_MAXPIDS);
     uint16_t ph = pid & ((1 << ph_bits) - 1);   // Placement handler
     // *******************************/
 
